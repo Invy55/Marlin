019ecf9f83f25caeadf7823b64f9104d1821630b
Marco <38048104+Invy55@users.noreply.github.com>
2023-11-13 16:39:09 +0100

    test fix for the orrible y assis shift

--- Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_common.h
+++ Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_common.h
@@ -75,10 +75,14 @@
 #define X_ENABLE_PIN                        PE4
 #define X_STEP_PIN                          PE3
 #define X_DIR_PIN                           PE2
-
+/*
 #define Y_ENABLE_PIN                        PE1
 #define Y_STEP_PIN                          PE0
 #define Y_DIR_PIN                           PB9
+*/
+#define Y_ENABLE_PIN                        PA3
+#define Y_STEP_PIN                          PA6
+#define Y_DIR_PIN                           PA1
 
 #define Z_ENABLE_PIN                        PB8
 #define Z_STEP_PIN                          PB5
@@ -87,10 +91,14 @@
 #define E0_ENABLE_PIN                       PB3
 #define E0_STEP_PIN                         PD6
 #define E0_DIR_PIN                          PD3
-
+/*
 #define E1_ENABLE_PIN                       PA3
 #define E1_STEP_PIN                         PA6
 #define E1_DIR_PIN                          PA1
+*/
+#define E1_ENABLE_PIN                       PE1
+#define E1_STEP_PIN                         PE0
+#define E1_DIR_PIN                          PB9
 
 //
 // Temperature Sensors
